3a2d58fafdb8200966183a86a8d5f6459f67a703
adam-labbane <adamlabbane.dev@gmail.com>
2025-07-08 22:28:25 +0200

    test

--- Backend/tests/conftest.py
+++ Backend/tests/conftest.py
@@ -1,7 +1,15 @@
 # Backend/tests/conftest.py
-import pytest
+import sys
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
+
 from fastapi.testclient import TestClient
 from main import app
+
+import pytest
 
 @pytest.fixture(scope="module")
 def test_client():
